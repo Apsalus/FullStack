2c9474350dc97eaf99f8c8e9b191dc7bf8f7ad71
dongjun <allnight724@gmail.com>
2023-11-02 22:31:13 +0900

    Middlewares

--- src/server.js
+++ src/server.js
@@ -7,12 +7,27 @@ const app = express();
 const handelHome = (req,res) => {
     return res.send("I still love you.");
 }
-const handelLogin = (req,res) => {
-    return res.send("Login here.");
+const logger = (req,res,next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
 }
 
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    next();
+}
+
+const handelProtected = (req,res) => {
+    return res.send("Welcome to the private lounge.");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handelHome);
-app.get("/login", handelLogin);
+app.get("/protected" ,handelProtected);
 
 const handelListening = () =>{
     console.log(`✅Server listenting on port http://localhost:${PORT}💥`);
