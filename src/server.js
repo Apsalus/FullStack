95004de4546ec7f865d1ff30868917c8fa3eb617
dongjun <allnight724@gmail.com>
2023-11-02 19:50:38 +0900

    GetRequests

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const handelHome = (req,res) => {
+    return res.send("I still love you.");
+}
+const handelLogin = (req,res) => {
+    return res.send("Login here.");
+}
+
+app.get("/", handelHome);
+app.get("/login", handelLogin);
+
 const handelListening = () =>{
     console.log(`✅Server listenting on port http://localhost:${PORT}💥`);
 }
