6e5f9265e3653e71f1d0cedb36c978f6130b2650
dongjun <allnight724@gmail.com>
2023-11-22 21:20:00 +0900

    Statics

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+    
 const videoSchema = new mongoose.Schema({
     title: { type: String, require:true, trim: true, maxLength:80 },
     description: { type: String, require:true, trim: true, minLength:20 },
@@ -10,6 +10,13 @@ const videoSchema = new mongoose.Schema({
         rating: { type: Number, defalult: 0 , require: true },
     },
 });
+
+videoSchema.static('formatHashtags', function(hashtags){
+    return hashtags
+    .split(",")
+    .map((word)=> (word.startsWith("#") ? word : `#${word}`))
+})
+
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
